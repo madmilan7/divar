bd659044c15dad6d7776900f93019f1d14956b72
Mahdi Bagheri <qarjis8@gmail.com>
2024-05-12 00:46:18 +0330

    set accessToken in headers

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,12 +1,23 @@
 import { Route, Routes } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 
 import PageNotFound from "pages/404";
 import AdminPage from "pages/AdminPage";
 import AuthPage from "pages/AuthPage";
 import DashboardPage from "pages/DashboardPage";
 import HomePage from "pages/HomePage";
+import { getProfile } from "services/user";
 
 function Router() {
+  const { data, isPending, error } = useQuery({
+    queryKey: ["profile"],
+    queryFn: getProfile,
+  });
+  
+  if (isPending) return <h1>loading...</h1>
+
+  if (error) return <h1>{error}</h1>
+
   return (
     <Routes>
       <Route index element={<HomePage />} />
